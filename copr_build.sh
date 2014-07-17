09277b73a1f94bbd403f8fa5a7f1cb195163ff23
Mat Booth <mat.booth@redhat.com>
2014-07-17 13:45:32 +0100

    Minor improvements to copr build script

--- copr_build.sh
+++ copr_build.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/bash
+set -e
 
+#
+# This script builds a package for the Eclipse Luna SCL
+#
+# Should be run from a cloned dist-git repo of a package (instead of "fedpkg build")
+#
+
+# Get an NVR with the SCL name prefixed
 VERREL=`fedpkg --dist f21 verrel 2>/dev/null`
+VERREL=eclipse-luna-${VERREL#eclipse-luna-}
+
+# Generate an SRPM
 fedpkg --dist f21 srpm 2>/dev/null
 
+# Upload and build
 scp ${VERREL}.src.rpm ${USER}@fedorapeople.org:~/public_html/copr/.
 copr-cli build --nowait eclipse-luna http://${USER}.fedorapeople.org/copr/${VERREL}.src.rpm
 
