c3759fb101778c4ace06be9d3edb1effe05142b5
Mat Booth <mat.booth@redhat.com>
2014-11-13 12:42:27 +0000

    Make repo a bit more collection agnostic

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-eclipse-luna
-============
+# eclipse-metapackage
 
-This repository contains the source for the Eclipse Luna Software Collection meta-package.
+This repository contains the source for the Eclipse Software Collections meta-packages.
+
+### Eclipse Mars for Fedora 21
+
+https://copr.fedoraproject.org/coprs/mbooth/eclipse-mars/
+
+### Eclipse Luna for Fedora 20
+
+https://copr.fedoraproject.org/coprs/mbooth/eclipse-luna/
--- copr_build.sh
+++ copr_build.sh
@@ -2,24 +2,30 @@
 set -e
 
 #
-# This script builds a package for the Eclipse Luna SCL
+# This script builds a package for the SCL named below
 #
 # Should be run from a cloned dist-git repo of a package (instead of "fedpkg build")
 #
 
-echo "%scl eclipse-luna" > macros.eclipse-luna-config
-echo "%scl_vendor rh" >> macros.eclipse-luna-config
-sudo mv macros.eclipse-luna-config /etc/rpm/macros.eclipse-luna-config
+# Name of the collection we are building for
+COLLECTION=eclipse-luna
+
+# Set some build-time macros for the SCL
+BUILD_MACRO_FILE=macros.eclipse-metapackage-config
+echo "%scl $COLLECTION" > $BUILD_MACRO_FILE
+echo "%scl_vendor rh" >> $BUILD_MACRO_FILE
+sudo mv $BUILD_MACRO_FILE /etc/rpm/$BUILD_MACRO_FILE
 
 # Get an NVR with the SCL name prefixed
 VERREL=$(fedpkg --dist f21 verrel 2>/dev/null | grep $(basename $(pwd)))
-VERREL=eclipse-luna-${VERREL#eclipse-luna-}
+VERREL=$COLLECTION-${VERREL#$COLLECTION-}
 
 # Generate an SRPM
 fedpkg --dist f21 srpm 2>/dev/null
 
 # Upload and build
 scp ${VERREL}.src.rpm ${USER}@fedorapeople.org:~/public_html/copr/.
-copr-cli build --nowait "mbooth/eclipse-luna" http://${USER}.fedorapeople.org/copr/${VERREL}.src.rpm
+copr-cli build --nowait "mbooth/$COLLECTION" http://${USER}.fedorapeople.org/copr/${VERREL}.src.rpm
 
-sudo rm /etc/rpm/macros.eclipse-luna-config
+# Remove macros
+sudo rm /etc/rpm/$BUILD_MACRO_FILE
